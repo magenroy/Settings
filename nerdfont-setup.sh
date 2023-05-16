8872c3f751a24ce1c64dcc73200ca00c8cd21f10
Roy Magen <magenroy@gmail.com>
2023-05-16 15:33:49 -0400

    neovim and nerdfonts

---
+++ nerdfont-setup.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+nerdfont=$1
+release=$2
+
+__get_nerdfont() {
+    set -e
+    set -u
+
+	echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v$release/$nerdfont.tar.xz"
+
+	ext=".tar.xz"
+
+    curl -fsSLo "$nerdfont$ext" \
+	"https://github.com/ryanoasis/nerd-fonts/releases/download/v$release/$nerdfont$ext"
+    tar xvf "$nerdfont$ext"
+    rm "$nerdfont$ext"
+}
+
+nerdfont="Inconsolata"
+release="3.0.1"
+
+fontdir=""
+if [ -e "$HOME/Library/Fonts" ]; then
+	# OS X
+	fontdir="Library/Fonts/"
+	cd "~/$fontdir"
+	__get_nerdfont
+else
+	# Linux
+	fontdir=.local/share/fonts
+	mkdir -p $HOME/$fontdir
+	cd "$HOME/$fontdir"
+	__get_nerdfont
+fi
+
+echo "Installed $nerdfont to ~/$fontdir"
+
+fc-cache -fv
